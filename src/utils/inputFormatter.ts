11fb60f06e9e4458ce34364731c80f2591c711c4
FranciscoPontes <57417441+FranciscoPontes@users.noreply.github.com>
2021-09-05 20:54:31 +0100

    Update inputFormatter.ts

--- src/utils/inputFormatter.ts
+++ src/utils/inputFormatter.ts
@@ -2,7 +2,7 @@ export const formatCode = (input: string): string =>
   input.toUpperCase().replace(' ', '_')
 
 export const formatLabel = (input: string): string => {
-  const lowerCasedInput: string = input.toLowerCase().replace('_', ' ')
+  const lowerCasedInput: string = input.toLowerCase().replace(/_*/g, ' ')
   const firstChar: string = lowerCasedInput.charAt(0).toUpperCase()
   return firstChar + lowerCasedInput.slice(1)
 }
